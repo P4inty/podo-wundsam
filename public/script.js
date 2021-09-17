7fd034bef55b299361c90bea62d8239970784f75
P4inty <martin.wundsam@gmail.com>
2021-09-17 16:54:27 +0200

    add cookie disclaimer

---
+++ public/script.js
@@ -0,0 +1,18 @@
+function showCookieDisclaimer() {
+    return {
+        show: false,
+        cookieName: 'cookie_disclaimer',
+        setCookie(value) {
+            let date = new Date();
+            date.setTime(date.getTime() + (7 * 24 * 60 * 60 * 1000));
+            document.cookie = this.cookieName + '=' + value + ';expires=' + date.toGMTString() + ';path=/;SameSite=Strict';
+            this.show = false;
+            console.log('setCookie', value);
+        },
+        getCookie() {
+            const cookie = document.cookie.match(new RegExp('(^|;)\\s*' + this.cookieName + '=([^;]*)'));
+            this.show = cookie ? false : true;
+            console.log(this.show)
+        }
+    }
+}
